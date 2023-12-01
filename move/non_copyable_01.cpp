70c14362e77c9c84edb9a386c69607ad2c85d609
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-12-01 19:16:09 +0300

    Update non_copyable_01.cpp

--- move/non_copyable_01.cpp
+++ move/non_copyable_01.cpp
@@ -1,12 +1,12 @@
-#include "triple.h"
+#include <string>
 #include <memory>
 
 int main()
 {
-	auto upx = std::make_unique<Triple>(10, 30, 56);
-
-	//auto upy{ upx }; //gecersiz
-	//auto upy = upx;
-	//auto upy(upx);
-	//unique_ptr<Triple> upy = upx;
+	auto upx = std::make_unique<std::string>(20, 'E');
+	
+	auto up1{ upx };  //error
+	auto up2 = upx;  //error
+	auto up3(upx);   //error
+	std::unique_ptr<std::string> up4 = upx; //error
 }
