c958566befc184f41e62029b9ed2c90b1aae4475
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2023-12-01 19:09:11 +0300

    Update error_02.cpp

--- errors/error_02.cpp
+++ errors/error_02.cpp
@@ -1,13 +1,15 @@
-#include "triple.h"
+#include "point.h"
 #include <memory>
+#include <iostream>
+
 
 int main()
 {
-	auto upx = std::make_unique<Triple>(1, 2, 3);
+	auto upx = std::make_unique<Point>(1, 2, 3);
 	std::cout << "upx = " << (upx ? "not empty" : "empty") << '\n';
 	auto ptr = upx.release();
 	std::cout << "upx = " << (upx ? "not empty" : "empty") << '\n';
 	std::cout << *ptr << '\n';
-	std::cout << std::boolalpha << (upx == nullptr) << '\n'; 
+	std::cout << std::boolalpha << (upx == nullptr) << '\n';
 	std::cout << *upx << '\n'; //ub
 }
