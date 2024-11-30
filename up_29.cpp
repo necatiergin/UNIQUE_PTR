ae17394be6adb4b96db0ef07fdeaba694ab6e851
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-11-30 21:39:08 +0300

    Update up_29.cpp

--- up_29.cpp
+++ up_29.cpp
@@ -1,15 +1,15 @@
 #include <iostream>
-#include "triple.h"
+#include "point.h"
 
 int main()
 {
-	auto upx = std::make_unique<Triple>(10, 30, 56);
+	auto upx = std::make_unique<Point>(10, 30, 56);
 
 	std::cout << "upx = " << (upx ? "not empty" : "empty") << '\n';
 
 	auto upy = move(upx);
 
-	(void)getchar();
+	(void)std::getchar();
 
 	std::cout << "upx = " << (upx ? "not empty" : "empty") << '\n';
 	std::cout << "upy = " << (upy ? "not empty" : "empty") << '\n';
