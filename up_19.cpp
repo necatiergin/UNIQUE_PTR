3bf2626874f39369bdc3a755b7f30fac22e339a0
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-12-25 19:06:01 +0300

    Update up_19.cpp

--- up_19.cpp
+++ up_19.cpp
@@ -5,14 +5,14 @@
 
 std::unique_ptr<std::string> foo()
 {
-	return new std::string{"necati"}; //invalid
+	return new std::string{ "necati" }; //invalid
 }
 
 void bar(std::unique_ptr<std::string>);
 
 int main()
 {
-	using namespace std::unique_ptr, std::string;
+	using std::unique_ptr, std::string;
 
 	//unique_ptr<string> ux = new string; //invalid
 	unique_ptr<string> ux(new string); //valid
