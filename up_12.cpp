5ac84fd17d237cc6a59efeac983f4feafc72a96b
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-12-25 18:29:15 +0300

    Update up_12.cpp

--- up_12.cpp
+++ up_12.cpp
@@ -4,17 +4,17 @@
 
 int main()
 {
-	using namespace std;
+	using std::string, std::cout;
 
 	auto f = [](string* p) {
 		cout << "lambda deleter deletes the string at the address " << p << '\n';
 		delete p;
-	};
+		};
 
-	unique_ptr<string, decltype(f)> uptr1{ new string{"Necati"}, f };
-	unique_ptr<string, decltype(f)> uptr2{ new string{"Necati"} }; //C++20
-	unique_ptr<string, decltype([](string* p) {
+	std::unique_ptr<string, decltype(f)> uptr1{ new string{"Necati"}, f };
+	std::unique_ptr<string, decltype(f)> uptr2{ new string{"Necati"} }; //C++20
+	std::unique_ptr < string, decltype([](string* p) {
 		cout << "lambda deleter deletes the string at the address " << p << '\n';
 		delete p;
-		})> uptr3{ new string{"Necati"} }; //C++20
+		}) > uptr3{ new string{"Necati"} }; //C++20
 }
