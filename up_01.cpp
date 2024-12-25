cb97b22cfc4ec4f949caadfaf7312197af003c2b
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-12-25 18:08:37 +0300

    Update up_01.cpp

--- up_01.cpp
+++ up_01.cpp
@@ -4,8 +4,8 @@
 
 int main()
 {
-	using namespace std;
+	using std::cout, std::string, std::unique_ptr;
 
-	cout << "sizeof(string *)           = " << sizeof(string *) << '\n';
+	cout << "sizeof(string *)           = " << sizeof(string*) << '\n';
 	cout << "sizeof(unique_ptr<string>) = " << sizeof(unique_ptr<string>) << '\n';
 }
