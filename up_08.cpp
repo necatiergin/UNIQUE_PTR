ea11ac1dc89cae5840c833faec3f3aeba90c899a
Necati Ergin <46518309+necatiergin@users.noreply.github.com>
2024-12-25 18:23:16 +0300

    Update up_08.cpp

--- up_08.cpp
+++ up_08.cpp
@@ -1,12 +1,42 @@
-#include <memory>
-#include "point.h"
 #include <iostream>
+#include <memory>
 
 template<typename T, typename ...Args>
 std::unique_ptr<T> MakeUnique(Args && ...args)
 {
 	return std::unique_ptr<T> {new T(std::forward<Args>(args)...)};
 }
+
+class Point {
+public:
+	Point() = default;
+	Point(int x, int y, int z) : mx{x}, my{y}, mz{z} 
+	{
+		std::cout << "Point ctor\n"; 
+	}
+
+	~Point()
+	{
+		std::cout << "Point dtor\n"; 
+	}
+
+	Point& set(int x, int y, int z)
+	{
+		mx = x;
+		my = y;
+		mz = z;
+		return *this;
+	}
+
+	friend std::ostream& operator<<(std::ostream& os, const Point& p)
+	{
+		return os << '(' << p.mx << ", " << p.my << ", " << p.mz << ')';
+	}
+
+private:
+	int mx{}, my{}, mz{};
+};
+
 
 int main()
 {
